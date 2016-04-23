70ab2d7dd7f25386d8c9ce2b56d46e6cc6b3eff4
Fakhry Ikhsan Firdaus <fakhry.ikhsan.firdaus@gmail.com>
2016-04-23 15:46:45 +0700

    First Commit

---
+++ app/src/main/java/net/crevion/fakhry/popularmovies/MoviesApiService.java
@@ -0,0 +1,13 @@
+package net.crevion.fakhry.popularmovies;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+
+
+/**
+ * Created by Fakhry on 23/04/2016.
+ */
+public interface MoviesApiService {
+    @GET("/movie/popular")
+    void getPopularMovies(Callback<Movie.MovieResult> cb);
+}
